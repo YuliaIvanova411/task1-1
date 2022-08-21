ce6ee608bb0e9967373267f8b88d2f4ff3686fd8
Yulia Ivanova <Yulia.Ivanova411@gmail.com>
2022-08-21 13:14:22 +0300

    kopeyki

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ fun main() {
     val amount = 10000_00
     val commissionRate = 0.0075
     val minCommission = 35_00
-    val result = if (amount * commissionRate < minCommission) minCommission/100 else (amount * commissionRate)/100
-    println("Комиссия за перевод: " + result + " руб.")
+    val result = if (amount * commissionRate < minCommission) minCommission else (amount * commissionRate)
+    println("Комиссия за перевод: " + result + " коп.")
 
     }
